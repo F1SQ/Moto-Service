e33aa10dab0a6ff1d058893f102bab9aa0a4c8fb
denis <ridchenkodenis@gmail.com>
2022-02-04 05:26:49 +0200

    create: section-price

--- js/common.js
+++ js/common.js
@@ -9,4 +9,27 @@ $(function() {
 		hideScrollbar: false,
   });
 
+
+	let priceName = document.querySelectorAll('.price-name');
+	let	priceItem = document.querySelectorAll('.price-item');
+	let priceTable = document.querySelector('.price-table');
+
+	priceName.forEach((item, i) => {		
+		item.addEventListener('mouseenter', (event) => {
+			event.target.parentElement.classList.add('price-item_visible');
+			event.target.classList.add('price-name_active');
+			priceTable.classList.add('price-table_visible');
+		});
+
+	
+	});  
+
+	// priceItem.forEach((item, i) => {
+	// 		item.addEventListener('mouseleave', (event) => {
+	// 		event.target.parentElement.classList.remove('price-item_visible');
+
+	// 	});
+	// });
+
+
 });
