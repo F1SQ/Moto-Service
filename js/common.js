9d89fc9cf999cba85f6f219fdcb8117cbdcb4dac
denis <ridchenkodenis@gmail.com>
2022-02-04 05:47:20 +0200

    edit: fixed html+js for price, price-table

--- js/common.js
+++ js/common.js
@@ -12,16 +12,17 @@ $(function() {
 
 	let priceName = document.querySelectorAll('.price-name');
 	let	priceItem = document.querySelectorAll('.price-item');
-	let priceTable = document.querySelector('.price-table');
+	let priceTable = document.querySelectorAll('.price-table');
+	
 
 	priceName.forEach((item, i) => {		
 		item.addEventListener('mouseenter', (event) => {
 			event.target.parentElement.classList.add('price-item_visible');
 			event.target.classList.add('price-name_active');
-			priceTable.classList.add('price-table_visible');
+					
+		
 		});
 
-	
 	});  
 
 	// priceItem.forEach((item, i) => {
